11ecdacf67f4a17488522d6b4308ab07a72ca613
Brian <bybyby@gmail.com>
2021-03-07 17:12:04 -0800

    Break out into Components for Avatar and Detail

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,22 +9,22 @@ function App() {
 
       <Card
         name={contacts[0].name}
-        imgURL={contacts[0].imgURL}
-        phone={contacts[0].phone}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
         email={contacts[0].email}
       />
 
       <Card
         name={contacts[1].name}
-        imgURL={contacts[1].imgURL}
-        phone={contacts[1].phone}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
         email={contacts[1].email}
       />
 
       <Card
         name={contacts[2].name}
-        imgURL={contacts[2].imgURL}
-        phone={contacts[2].phone}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
         email={contacts[2].email}
       />
     </div>
