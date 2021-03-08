27b18966e0f1ad431074dddedc8491e4575a9bd2
Brian <bybyby@gmail.com>
2021-03-07 20:52:34 -0800

    Map data for Card components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,31 +2,24 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id} // React must have prop `key` with unique value assigned for each Card component
+      myId={contact.id} // Pass id as another prop if need to use value
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(createCard)}
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,6 +6,8 @@ function Card(props) {
   return (
     <div className="card">
       <div className="top">
+        <p>{props.myId}</p>
+
         <h2 className="name">{props.name}</h2>
         <Avatar img={props.img} />
       </div>
